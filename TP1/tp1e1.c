ece2c13d512fa222fe3ce3f0ac5646cac94d95ac
Miguel <miguelignacioiglesias@gmail.com>
2022-05-30 18:59:38 -0300

    Primer ejercicio

---
+++ TP1/tp1e1.c
@@ -0,0 +1,22 @@
+/*
+ Enunciado:
+  Una empresa desea calcular el sueldo de un empleado e imprimir su recibo detallando:
+  Nombre y Apellido, Sueldo Básico, Premio, Comida, Viáticos, Ausentes, Obra social (3%),
+  Ley 19032 (3%), y Jubilación (11%).
+  Por los ausentes, se descuenta un monto fijo por día de ausencia. Los
+  porcentajes se calculan sobre los montos remunerativos. Viáticos, premios y
+  comida no se consideran remunerativos.
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv[])
+{
+
+
+
+
+
+	return 0;
+}
+
